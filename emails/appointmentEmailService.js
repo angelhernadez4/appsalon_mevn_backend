63e62c8d03de41fd5962af6c47d057e71e536dbc
angelhernandez <angel.lphernandez@gmail.com>
2024-12-17 09:23:00 -0600

    Fix the service emails.

--- emails/appointmentEmailService.js
+++ emails/appointmentEmailService.js
@@ -5,15 +5,21 @@ export async function sendEmailNewAppointment({ date, time, fullname }) {
 
     // Enviar el email
     const info = await transporter.sendMail({
-        from: 'Appsalon <citas@appsalon.com>',
-        to: 'admin@appsalon.com',
+        from: `"Appsalon" <${process.env.FRONTEND_URL}>`,
+        to: `${process.env.EMAIL_USER}`,
         subject: "Appsalon - Nueva cita",
         text: "Appsalon - Nueva cita",
-        html: `<body style="background-color: rgb(31 41 55);">
-        <h1 style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 800; font-size: 3rem; text-align: center;">Appsalon - Nueva cita</h1>
-        <p style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 400; font-size: 1.2rem;">Hola: Admin, tienes una nueva cita de ${fullname}</p>
-        <p style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 400; font-size: 1.2rem;">La cita será el día: ${date} a las ${formatTime(time)} horas.</p>
-        </body>`
+        html: `
+            <body style="background-color: rgb(31, 41, 55); margin: 0; padding: 0; box-sizing: border-box;">
+                <h1 style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 800; font-size: 3rem; text-align: center; margin: 20px 0;">Appsalon - Nueva cita</h1>
+                <p style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 400; font-size: 1.2rem; text-align: center; margin: 10px 0;">
+                    Hola: Admin, tienes una nueva cita de ${fullname}
+                </p>
+                <p style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 400; font-size: 1.2rem; text-align: center; margin: 10px 0;">
+                    La cita será el día: ${date} a las ${formatTime(time)} horas.
+                </p>
+            </body>
+        `
     })
 }
 
@@ -22,15 +28,21 @@ export async function sendEmailUpdateAppointment({ date, time, fullname }) {
 
     // Enviar el email
     const info = await transporter.sendMail({
-        from: 'Appsalon <citas@appsalon.com>',
-        to: 'admin@appsalon.com',
+        from: `"Appsalon" <${process.env.FRONTEND_URL}>`,
+        to: `${process.env.EMAIL_USER}`,
         subject: "Appsalon - Cita actualizada",
         text: "Appsalon - Cita actualizada",
-        html: `<body style="background-color: rgb(31 41 55);">
-        <h1 style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 800; font-size: 3rem; text-align: center;">Appsalon - Cita actualizada</h1>
-        <p style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 400; font-size: 1.2rem;">Hola: Admin, ${fullname} a modificado una cita</p>
-        <p style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 400; font-size: 1.2rem;">La nueva cita será el día: ${date} a las ${formatTime(time)} horas.</p>
-        </body>`
+        html: `
+            <body style="background-color: rgb(31, 41, 55); margin: 0; padding: 0; box-sizing: border-box;">
+                <h1 style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 800; font-size: 3rem; text-align: center; margin: 20px 0;">Appsalon - Cita actualizada</h1>
+                <p style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 400; font-size: 1.2rem; text-align: center; margin: 10px 0;">
+                    Hola: Admin, ${fullname} a modificado una cita
+                </p>
+                <p style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 400; font-size: 1.2rem; text-align: center; margin: 10px 0;">
+                    La nueva cita será el día: ${date} a las ${formatTime(time)} horas.
+                </p>
+            </body>
+        `
     })
 }
 
@@ -39,13 +51,19 @@ export async function sendEmailCancelAppointment({ date, time, fullname }) {
 
     // Enviar el email
     const info = await transporter.sendMail({
-        from: 'Appsalon <citas@appsalon.com>',
-        to: 'admin@appsalon.com',
+        from: `"Appsalon" <${process.env.FRONTEND_URL}>`,
+        to: `${process.env.EMAIL_USER}`,
         subject: "Appsalon - Cita cancelada",
         text: "Appsalon - Cita cancelada",
-        html: `<body style="background-color: rgb(31 41 55);">
-        <h1 style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 800; font-size: 3rem; text-align: center;">Appsalon - Cita cancelada</h1>
-        <p style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 400; font-size: 1.2rem;">Hola: Admin, ${fullname} a cancelado una cita</p>
-        <p style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 400; font-size: 1.2rem;">La cita estaba programada para el día: ${date} a las ${formatTime(time)} horas.</p>`
+        html: `
+        <body style="background-color: rgb(31, 41, 55); margin: 0; padding: 0; box-sizing: border-box;">
+            <h1 style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 800; font-size: 3rem; text-align: center; text-align: center; margin: 20px 0;">Appsalon - Cita cancelada</h1>
+            <p style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 400; font-size: 1.2rem; text-align: center; margin: 10px 0;">
+                Hola: Admin, ${fullname} a cancelado una cita
+            </p>
+            <p style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 400; font-size: 1.2rem; text-align: center; margin: 10px 0;">
+                La cita estaba programada para el día: ${date} a las ${formatTime(time)} horas.
+            </p>
+        </body>`
     })
 }
--- emails/authEmailService.js
+++ emails/authEmailService.js
@@ -5,19 +5,30 @@ export async function sendEmailVerification({ name, email, token }) {
 
     // Enviar el email
     const info = await transporter.sendMail({
-        from: 'Appsalon <cuentas@appsalon.com>',
+        from: `"Appsalon" <${process.env.EMAIL_USER}>`,
         to: email,
         subject: "Appsalon - Confirma tu cuenta",
         text: "Appsalon - Confirma tu cuenta",
-        html: `<body style="background-color: rgb(31 41 55);">
-        <h1 style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 800; font-size: 3rem; text-align: center;">Appsalon - Confirma tu cuenta</h1>
-        <p style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 400; font-size: 1.2rem;">Hola: ${name}, confirma tu cuenta en Appsalon</p>
-        <p style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 400; font-size: 1.2rem;">Tu cuenta esta casi lista, solo debes confirmarla en el siguiente enlace</p>
-        <div style="display: flex; align-items: center; justify-content: center;">
-            <a style="box-shadow: rgb(38, 57, 77) 0px 20px 30px -10px; text-decoration: none; background-color: rgb(59 130 246); border:none; font-family: ui-sans-serif, system-ui, sans-serif; color: white; padding: 1rem; border-radius: .5rem; font-size: 1.1rem;" href="${process.env.FRONTEND_URL}/auth/confirm-account/${token}">Confirmar cuenta</a>
-        </div>
-        <p style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 400; font-size: 1.2rem;">Si tu no creaste esta cuenta, puedes ignorar este mensaje.</p>
-        </body>`
+        html: `
+            <body style="background-color: rgb(31, 41, 55); margin: 0; padding: 0; box-sizing: border-box;">
+                <h1 style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 800; font-size: 3rem; text-align: center; margin: 20px 0;">Appsalon - Confirma tu cuenta</h1>
+                <p style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 400; font-size: 1.2rem; text-align: center; margin: 10px 0;">
+                    Hola <strong>${name}</strong>, confirma tu cuenta en Appsalon.
+                </p>
+                <p style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 400; font-size: 1.2rem; text-align: center; margin: 10px 0;">
+                    Tu cuenta está casi lista. Solo debes confirmarla en el siguiente enlace:
+                </p>
+                <div style="display: flex; justify-content: center; margin: 20px;">
+                    <a href="${process.env.FRONTEND_URL}/auth/confirm-account/${token}"
+                        style="text-decoration: none; background-color: rgb(59, 130, 246); color: white; padding: 1rem 2rem; border-radius: 0.5rem; font-size: 1.1rem; box-shadow: rgb(38, 57, 77) 0px 20px 30px -10px;">
+                        Confirmar cuenta
+                    </a>
+                </div>
+                <p style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 400; font-size: 1.2rem; text-align: center; margin: 10px 0;">
+                    Si tú no creaste esta cuenta, puedes ignorar este mensaje.
+                </p>
+            </body>
+        `
     })
 }
 
@@ -26,18 +37,26 @@ export async function sendEmailPasswordReset({ name, email, token }) {
 
     // Enviar el email
     const info = await transporter.sendMail({
-        from: 'Appsalon <cuentas@appsalon.com>',
+        from: `"Appsalon" <${process.env.EMAIL_USER}>`,
         to: email,
         subject: "Appsalon - Reestablece tu contraseña",
         text: "Appsalon - Reestablece tu contraseña",
-        html: `<body style="background-color: rgb(31 41 55);">
-        <h1 style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 800; font-size: 3rem; text-align: center;">Appsalon - Reestablece tu contraseña</h1>
-        <p style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 400; font-size: 1.2rem;">Hola: ${name}, has solicistado reestablecer tu contraseña</p>
-        <p style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 400; font-size: 1.2rem;">Sigue el siguiente enlace para generar una nueva contraseña</p>
-        <div style="display: flex; align-items: center; justify-content: center;">
-            <a style="box-shadow: rgb(38, 57, 77) 0px 20px 30px -10px; text-decoration: none; background-color: rgb(59 130 246); border:none; font-family: ui-sans-serif, system-ui, sans-serif; color: white; padding: 1rem; border-radius: .5rem; font-size: 1.1rem;" href="${process.env.FRONTEND_URL}/auth/forgot-password/${token}">Reestablecer contraseña</a>
-        </div>
-            <p style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 400; font-size: 1.2rem;">Si tu no solicitaste esto, puedes ignorar este mensaje.</p>
+        html: `
+            <body style="background-color: rgb(31, 41, 55); margin: 0; padding: 0; box-sizing: border-box;">
+                <h1 style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 800; font-size: 3rem; text-align: center; margin: 20px 0;">Appsalon - Reestablece tu contraseña</h1>
+                <p style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 400; font-size: 1.2rem; text-align: center; margin: 10px 0;">
+                    Hola: ${name}, has solicistado reestablecer tu contraseña
+                </p>
+                <p style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 400; font-size: 1.2rem; text-align: center; margin: 10px 0;">
+                    Sigue el siguiente enlace para generar una nueva contraseña
+                </p>
+                <div style="display: flex; align-items: center; justify-content: center; margin: 20px;"">
+                    <a href="${process.env.FRONTEND_URL}/auth/forgot-password/${token}" 
+                        style="text-decoration: none; background-color: rgb(59, 130, 246); font-family: ui-sans-serif, system-ui, sans-serif; color: white; padding: 1rem 2rem; border-radius: .5rem; font-size: 1.1rem; box-shadow: rgb(38, 57, 77) 0px 20px 30px -10px;">Reestablecer contraseña</a>
+                </div>
+                <p style="font-family: ui-sans-serif, system-ui, sans-serif; color: white; font-weight: 400; font-size: 1.2rem; text-align: center; margin: 10px 0;">
+                    Si tu no solicitaste esto, puedes ignorar este mensaje.
+                </p>
         </body>`
     })
 }
